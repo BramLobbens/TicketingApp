c2fccc01852bb13d4f12bd34a4fd8ea7024628e3
BramLobbens <lobbensbram@gmail.com>
2020-12-19 20:59:35 +0100

    initial react app commit

---
+++ ticket-app/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { TicketForm } from './components';
+
+export default function App() {
+  return (
+    <>
+      <TicketForm />
+    </>
+    );
+}
---
+++ ticket-app/src/components/ticketform/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {} from './styles/ticketform';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        title: '',
+        content: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    const url = 'https://localhost:5001/api/ticket';
+
+    const ticket = {
+        'title': this.state.title,
+        'content': this.state.content,
+        'postedOn': new Date()
+    };
+
+    const request = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify(ticket),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    });
+
+    fetch(request)
+        .then(res => res.json())
+        .then(res => console.log(res));
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label for="title">Title:</label>
+        <input name="title" type="text" value={this.state.title} onChange={this.handleChange} />
+        <label for="content">Content:</label>
+        <textarea name="content" value={this.state.content} onChange={this.handleChange} placeholder="My ticket details..." />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default function TicketForm(props) {
+    return (
+        <Form />
+    );
+}
