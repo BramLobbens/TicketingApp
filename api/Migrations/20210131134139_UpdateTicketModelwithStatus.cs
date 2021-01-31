73c470b34a215fb8fa1d1d2304c68215c6c6bf18
BramLobbens <lobbensbram@gmail.com>
2021-01-31 20:26:43 +0100

    add update migration ticketstatus

---
+++ api/Migrations/20210131134139_UpdateTicketModelwithStatus.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class UpdateTicketModelwithStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Ticket");
+        }
+    }
+}
